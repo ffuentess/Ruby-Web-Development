ac465c7369d96b100720fe079e845857b6e53605
ffuentess <ffuentessp@gmail.com>
2021-04-11 09:06:46 -0400

    Create ejercicios-bucles-for.js

---
+++ ejercicios-bucles-for.js
@@ -0,0 +1,24 @@
+for (var a = 0; a < 8; a++) {   
+    console.log(a);
+}
+
+//valor de salida en la consola: "0,1,2,3,4,5,6,7"//
+
+for (var b = 10; b < 100; b += 10) {   
+    console.log(b);
+}
+
+//valor de salida en la consola: "10,20,30,40,50,60,70,80,90"//
+
+for (var c = 16; c > 0; c--) {
+        console.log(c);
+}
+
+//valor de salida en la consola: "16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1"//
+
+for (var d = 0; d < 100; d++) {
+        console.log(d);
+        d += 5;
+}
+
+//valor de salida en la consola: "0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96"
